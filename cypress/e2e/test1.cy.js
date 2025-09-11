c7cdf0cfa4480ac6ec2f893396f56ed7507533ea
Bogdan <>
2025-09-11 17:56:18 +0300

    select only visible items

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,5 +1,11 @@
+/// <reference types="Cypress" />
+
+
 describe('My first test', () => {
   it('My first test case', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('input[type="search"]').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length', 4)
   })
 })
