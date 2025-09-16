70a4c2792fd1ba1a2a4714da92b03577e1d7631c
Bogdan <>
2025-09-16 19:15:44 +0300

    parent child chining find eq contains

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -7,5 +7,10 @@ describe('My first test', () => {
     cy.get('input[type="search"]').type('ca')
     cy.wait(2000)
     cy.get('.product:visible').should('have.length', 4)
+
+    //parent child chaining
+    cy.get('.products').find('.product').should('have.length', 4)
+
+    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
   })
 })
